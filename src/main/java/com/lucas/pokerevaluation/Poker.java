1c2cbe4584f94f73fd493892af3ae5c74ece33ca
Luke <luke@github.com>
2014-04-12 15:28:17 -0400

    2014-04-12 Test Commit 2

--- src/main/java/com/lucas/pokerevaluation/Poker.java
+++ src/main/java/com/lucas/pokerevaluation/Poker.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Poker {
 
 	public static void main(String[] args) {
-		//Test Commit
+		//Test Commit2
 		System.out.println("Hello world");
 		args=new String[]{"-h","7d","4c","8c", "6h", "5s"};
 		List<String> segments = new ArrayList<String>();
