25d6b52483ccdc24154ea88c9532b52b50f0064a
Luke <lucasweatherby@github.com>
2014-04-13 12:11:42 -0400

    2014-04-13 Added Comments

--- src/main/java/com/lucas/pokerevaluation/Poker.java
+++ src/main/java/com/lucas/pokerevaluation/Poker.java
@@ -5,9 +5,16 @@ import com.lucas.pokerevaluation.beans.PokerHand;
 public class Poker {
 
 	public static void main(String[] args) {
-		args=new String[]{"Ad","2d","3d", "4c", "5d"};
+		//This is just one quick way to simulate args through eclipse
+		//args=new String[]{"Ad","2d","3d", "4c", "5d"};
+		
+		//Make The Object
 		PokerHand hand = new PokerHand();
+		
+		//Parse through the arguments
 		hand.parseFromInput(args);
+		
+		//Print out the hands rank
 		System.out.println(hand.getPokerHandRank());		
 	}
 
--- src/main/java/com/lucas/pokerevaluation/beans/PokerHand.java
+++ src/main/java/com/lucas/pokerevaluation/beans/PokerHand.java
@@ -35,10 +35,13 @@ public class PokerHand implements PokerHandRankValidator{
 	
 	public String getPokerHandRank()
 	{
+		//First we check to make sure that the hand isn't already invalid
 		if(this.rank == handRank.INVALID_INPUT)
 		{
 			return this.getRank().toString();
 		}
+		//Then we check for the hand rank by priority of Poker rules.
+		//If a hand rank is round it will be immediately returned
 		if(isRoyalFlush())
 		{
 			this.rank= handRank.ROYAL_FLUSH;
@@ -84,29 +87,45 @@ public class PokerHand implements PokerHandRankValidator{
 			this.rank= handRank.PAIR;
 			return this.getRank().toString();
 		}
+		//If we make it through all of the hand ranks,
+		//then we know the hand must be played as a high card.
 		this.rank = handRank.HIGH_CARD;
 		return this.getRank().toString() + " " +  this.getHighCard();
 	}
 	
 	public void parseFromInput(String[] args)
 	{
+		//Create a string list for holding the input arguments for manipulation
 		List<String> segments = new ArrayList<String>();
 		for (int i = 0; i < args.length; i++) 
 		{
 				segments.add(args[i]);
 		}
 		
+		//If the input does not contain the correct amount of cards return invalid input
 		if(segments.size() > 5 || segments.size() < 5)
 		{
 			this.rank = handRank.INVALID_INPUT;
 			return;
 		}
 		
+		//Iterate though the string list holding input to convert the data
+		//so that the program can properly read it
 		for (Iterator<String> iterator = segments.iterator(); iterator.hasNext();) {
 			String segmentName=  (String) iterator.next();
+			
+			//This string will hold the value of the card
 			String a = "";
+			
+			//This character will hold the suit of the card
 			Character b=null;
+			
+			//Create the card for the input argument that we are on
 			Card card = new Card();
+			
+			//Check to see that the segment length is the proper size
+			//it could only be a length of 3 if the card is a 10
+			//anything else would be improper input
 			if(segmentName.length() < 2 || segmentName.length() >3)
 			{
 				this.rank = handRank.INVALID_INPUT;
@@ -114,11 +133,13 @@ public class PokerHand implements PokerHandRankValidator{
 			}
 			else if(segmentName.length()==2)
 			{
+				//Read and store the values from the input
 				a = segmentName.substring(0, 1);
 				b = segmentName.charAt(1);
 			}
 			else if(segmentName.length()==3)
 			{
+				//Read and store the values from the input
 				a = segmentName.substring(0, 2);
 				b = segmentName.charAt(2);
 			}
@@ -128,6 +149,8 @@ public class PokerHand implements PokerHandRankValidator{
 				//System.out.println("This is a problem");
 			}
 			
+			//Switch case to store the actual value of the card object to the
+			//numeric representation of the input value
 			switch (a) 
 			{
 	            case "A":  card.setCardValue(14);break;
@@ -135,15 +158,19 @@ public class PokerHand implements PokerHandRankValidator{
 	            case "Q":  card.setCardValue(12);break;
 	            case "J":  card.setCardValue(11);break;
 	            default:
+	            	//If the user inputs a value that is not an interger and it reaches this point
+	            	//or he/she puts in a value that is not in the set of numbers 2-10,
+	            	//then an invalid_input status will be set for the hand
 	            	if(!isInteger(a) || Integer.parseInt(a) < 2 || Integer.parseInt(a) > 14)
 	            	{
 	            		this.rank = handRank.INVALID_INPUT;
 	        			return;
 	            	}
+	            	//otherwise the value will be parsed and set for the card
 	            	card.setCardValue(Integer.parseInt(a));
 	            	break;
 			}
-			
+			//Switch statement to set the suit of the card from the input
 			switch (b) 
 			{
 	            case 'c':  card.setSuit(suit.CLUBS);break;
@@ -151,20 +178,28 @@ public class PokerHand implements PokerHandRankValidator{
 	            case 'd':  card.setSuit(suit.DIAMONDS);break;
 	            case 's':  card.setSuit(suit.SPADES);break;
 	            default:
+	            	//if the suit is not one of the correct inputs it will return an
+	            	//invalid_input rank
 	            	this.rank = handRank.INVALID_INPUT;
         			return;
 			}
-			//System.out.println(card.getCardValue() + card.getSuit().toString());
 			cards.add(card);
 		}
 	}
 
 	@Override
 	public boolean isPair() {
+		//create an array ints to hold the count of each card number's occurrence
 		int[] rankCount = new int[15];
+		
+		//iterate through the cards in the hand
 		for (Card card : cards) {
+			//Increase that particular card number's occurrence
             rankCount[card.getCardValue()]++;
         }
+		
+		//Now loop through the array and see if any card has an occurence of two, is
+		//so, return true.
 		for (int i = 1; i < rankCount.length; i++) {
 			if(rankCount[i] == 2)
 				return true;
@@ -174,15 +209,21 @@ public class PokerHand implements PokerHandRankValidator{
 
 	@Override
 	public boolean isTwoPair() {
+		//create an array ints to hold the count of each card number's occurrence
 		int[] rankCount = new int[15];
+		//create an int for keeping a count of the number of pairs in a hand
 		int pairCount = 0;
+		//iterate through the cards in the hand
 		for (Card card : cards) {
+			//Increase that particular card number's occurrence
             rankCount[card.getCardValue()]++;
         }
+		//Now loop through the array and see if any card has an occurence of two
 		for (int i = 1; i < rankCount.length; i++) {
 			if(rankCount[i] == 2)
 				pairCount++;
 		}
+		//if the paircounter equals two, then we know we have to pair
 		if(pairCount==2)
 		{
 			return true;
@@ -192,10 +233,15 @@ public class PokerHand implements PokerHandRankValidator{
 
 	@Override
 	public boolean isThreeOfAKind() {
+		//create an array ints to hold the count of each card number's occurrence
 		int[] rankCount = new int[15];
+		//iterate through the cards in the hand
 		for (Card card : cards) {
+			//Increase that particular card number's occurrence
             rankCount[card.getCardValue()]++;
         }
+		//Now loop through the array and see if any card has an occurence of three, is
+		//so, return true.
 		for (int i = 1; i < rankCount.length; i++) {
 			if(rankCount[i] == 3)
 				return true;
@@ -205,23 +251,35 @@ public class PokerHand implements PokerHandRankValidator{
 
 	@Override
 	public boolean isStraight() {
+		//Need to create a list for storing the values of the cards in the hand
 		List<Integer> list = new ArrayList<Integer>();
-		for (Iterator iterator = cards.iterator(); iterator.hasNext();) {
+		
+		//Iterate through the cards in the hand
+		for (Iterator<Card> iterator = cards.iterator(); iterator.hasNext();) {
 			Card card = (Card) iterator.next();
+			//Add the value of the card to the newly created list
 			list.add(card.getCardValue());
 		}
+		
+		//Sort the list
 		Collections.sort( list );
 		//Functionality to test for low Ace Straight
 		//We know if the hand contains and Ace and a 2, then the ace must be low card
 		if(list.get(0) == 2 && list.get(list.size()-1) == 14)
 		{
+			//remove the high ace
 			list.remove(list.size()-1);
+			//add the low ace
 			list.add(1);
+			//Need to sort the list again
+			Collections.sort( list );
 		}
-		Collections.sort( list );
-		
+		//Iterate through the list and check the differences between consecutive cards
 		for (int i = 0; i < list.size()-1; i++) 
 		{
+			//If the difference between two consecutive cards is one for the entire hand
+			//then we know we have a straight. Otherwise, if any difference does not
+			//equal one, we know a straight cannot exist, so return false
             if (list.get(i) !=  list.get(i + 1) -1) 
             {
                 return false;
@@ -233,9 +291,16 @@ public class PokerHand implements PokerHandRankValidator{
 
 	@Override
 	public boolean isFlush() {
+		//We need to get the suit of the first card because all cards in 
+		//a flush must be the same. Thus we know by just checking that all
+		//cards match the first card's suit, we can successfully determine a flush
 		suit flushSuit = cards.get(0).getSuit();
-		for (Iterator iterator = cards.iterator(); iterator.hasNext();) {
+		
+		//Iterate through the list of cards in the hand
+		for (Iterator<Card> iterator = cards.iterator(); iterator.hasNext();) {
 			Card card = (Card) iterator.next();
+			//If any one card does not have the same suit, then we know that 
+			//hand is not a flush
 			if(!card.getSuit().equals(flushSuit))
 			{
 				return false;
@@ -247,18 +312,29 @@ public class PokerHand implements PokerHandRankValidator{
 
 	@Override
 	public boolean isFullHouse() {
+		//create an array ints to hold the count of each card number's occurrence
 		int[] rankCount = new int[15];
+		
+		//boolean for indicating whether or not the hand contains a pair
 		boolean pair = false;
+		//boolean for indicating whether or not the hand contains a three of a kind
 		boolean three = false;
+		
+		//iterate through the cards in the hand
 		for (Card card : cards) {
+			//Increase that particular card number's occurrence
             rankCount[card.getCardValue()]++;
         }
+		//Now loop through the array and see if any card has an occurence of three and two
 		for (int i = 1; i < rankCount.length; i++) {
+			//If an occurence of two is found, then we know we have a pair
 			if(rankCount[i] == 2)
 				pair = true;
+			//If an occurence of three is found, then we know we have a three of a kind
 			if(rankCount[i] == 3)
 				three = true;
 		}
+		//If we have both a pair and a three of a kind, we know we have a full house
 		if(pair && three)
 		{
 			return true;
@@ -268,10 +344,15 @@ public class PokerHand implements PokerHandRankValidator{
 
 	@Override
 	public boolean isFourOfAKind() {
+		//create an array ints to hold the count of each card number's occurrence
 		int[] rankCount = new int[15];
+		//iterate through the cards in the hand
 		for (Card card : cards) {
+			//Increase that particular card number's occurrence
             rankCount[card.getCardValue()]++;
         }
+		//Now loop through the array and see if any card has an occurence of four, if
+		//so, return true.
 		for (int i = 1; i < rankCount.length; i++) {
 			if(rankCount[i] == 4)
 				return true;
@@ -281,17 +362,27 @@ public class PokerHand implements PokerHandRankValidator{
 
 	@Override
 	public boolean isStraightFlush() {
+		//Here we know to be a straight flush, a hand must also be a straight and a flush
+		//so we can just test that preexisting functionality
 		return (isFlush() && isStraight());
 	}
 
 	@Override
 	public boolean isRoyalFlush() {
+		//Need to create a list for storing the values of the cards in the hand
 		List<Integer> list = new ArrayList<Integer>();
-		for (Iterator iterator = cards.iterator(); iterator.hasNext();) {
+		//Iterate through the cards in the hand
+		for (Iterator<Card> iterator = cards.iterator(); iterator.hasNext();) {
 			Card card = (Card) iterator.next();
+			//Add the value of the card to the newly created list
 			list.add(card.getCardValue());
 		}
+		//Sort the list
 		Collections.sort( list );
+		
+		//Here we know to be a royal flush you must be straight flush starting with a 10 ending
+		//with an ace
+		//so we can just test that preexisting functionality and look for 10 and ace
 		if(isFlush() && isStraight() && list.get(0)==10 && list.get(list.size()-1) == 14)
 		{
 			return true;
@@ -302,25 +393,33 @@ public class PokerHand implements PokerHandRankValidator{
 	@Override
 	public String getHighCard()
 	{
+		//Need to create a list for storing the values of the cards in the hand
 		List<Integer> list = new ArrayList<Integer>();
-		for (Iterator iterator = cards.iterator(); iterator.hasNext();) {
+		//Iterate through the cards in the hand
+		for (Iterator<Card> iterator = cards.iterator(); iterator.hasNext();) {
 			Card card = (Card) iterator.next();
+			//Add the value of the card to the newly created list
 			list.add(card.getCardValue());
 		}
+		//Sort the list
 		Collections.sort( list );
+		//Create a string for holding the high card
 		String highCard = list.get(list.size()-1).toString();
+		//Switch statement to convert back to lettered cards
 		switch (highCard) 
 		{
             case "14":  highCard = "A";break;
             case "13":  highCard = "K";break;
             case "12":  highCard = "Q";break;
             case "11":  highCard = "J";break;
-            default: 	highCard = highCard;break;
+            default: 	
+            	break;
 		}
 		return highCard;
 	}
 	public boolean isInteger(String s) {
         try {
+        	Integer.parseInt(s);
         } catch (NumberFormatException e) {
             return false;
         }
